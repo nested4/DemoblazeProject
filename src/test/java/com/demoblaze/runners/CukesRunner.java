88ab0d4e7f20930fcde8fe38be9cc1f83b8aa1ed
ihsan <111647146+nested4@users.noreply.github.com>
2022-12-22 19:14:11 +0300

    file structure updated

---
+++ src/test/java/com/demoblaze/runners/CukesRunner.java
@@ -0,0 +1,21 @@
+package com.demoblaze.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "html:target/default-html-reports.html",
+                "json:target/cucumber.json"
+        },
+        features = "src/test/resources/features",
+        glue = "com/demoblaze/step_definitions",
+        dryRun = true,
+        tags = ""
+
+)
+public class CukesRunner {
+
+}
